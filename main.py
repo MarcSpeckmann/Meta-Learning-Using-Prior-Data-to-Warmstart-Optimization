e2854df725fd6fd49e18fd21cb7c173b2850748d
Marc Speckmann <marc-speckmann@web.de>
2023-08-07 18:58:07 +0200

    Applying endless trials setting

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ if __name__ == "__main__":
     TEST = True  # If True, the best model is tested.
     RESUME = False  # If True, the experiment is resumed from a previous checkpoint. Else a new experiment is started.
     SEED = 42  # Seed for reproducibility
-    N_TRIALS = 2  # Number of trials to run. If -1, the number of trials is infinite.
+    N_TRIALS = -1  # Number of trials to run. If -1, the number of trials is infinite.
     WALLTIME_LIMIT = 6 * 60 * 60  # Time limit for the experiment in seconds. 6h
     MAX_EPOCHS = 20  # Maximum number of epochs to train for.
     IMG_SIZE = 32  # Image size to use for the model. (IMG_SIZE, IMG_SIZE)
