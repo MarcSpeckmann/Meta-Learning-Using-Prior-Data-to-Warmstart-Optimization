84667f827ab7f333f432121cc412621b52f44331
Marc Speckmann <marc-speckmann@web.de>
2023-08-11 11:37:01 +0200

    Adding template for utility function

---
+++ util.py
@@ -0,0 +1,13 @@
+class TemplateUtilityFunction:
+    def __init__():
+        pass
+
+    def utility(self, x, gp, y_max):
+        """_summary_
+
+        Args:
+            x (_type_): Array (size=(n_iter, bounds.shape[0]))
+            gp (_type_):  A gaussian process fitted to the relevant data.
+            y_max (_type_):  The current maximum known value of the target function.
+        """
+        pass
