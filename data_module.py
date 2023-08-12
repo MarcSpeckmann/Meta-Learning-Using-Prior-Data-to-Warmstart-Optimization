c57dd69a0af9b5e7fb31c29bbcbaa4d6ea7b8780
Marc Speckmann <marc-speckmann@web.de>
2023-08-12 15:20:28 +0200

    Adding filelook for sequencialising download on multiple trials

--- data_module.py
+++ data_module.py
@@ -9,6 +9,7 @@ from typing import Tuple
 
 import pytorch_lightning as pl
 import torch
+from filelock import FileLock
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -64,12 +65,13 @@ class DeepWeedsDataModule(pl.LightningDataModule):
         datadir = Path(os.path.join(self.data_path, DEEP_WOODS_DEFAULT_DIR))
         tar_path = Path(os.path.join(datadir, DEEP_WOODS_DEFAULT_TARBALL_PATH))
 
-        if not datadir.exists():
-            datadir.mkdir(parents=True)
+        with FileLock(datadir / ".data.lock"):
+            if not datadir.exists():
+                datadir.mkdir(parents=True)
 
-        self._download_deepweeds(url=DEEP_WOODS_LINK, dest=tar_path)
+            self._download_deepweeds(url=DEEP_WOODS_LINK, dest=tar_path)
 
-        self._unpack_tarball(tarball=tar_path, dest=datadir)
+            self._unpack_tarball(tarball=tar_path, dest=datadir)
 
     def setup(self, stage: str) -> None:
         """
@@ -302,3 +304,16 @@ class DeepWeedsDataModule(pl.LightningDataModule):
         test_dataset = ImageFolder(root=str(test_path), transform=self.pre_processing)
 
         return train_dataset, test_dataset
+
+
+if __name__ == "__main__":
+    data_model = DeepWeedsDataModule(
+        batch_size=32,
+        img_size=(32, 32),
+        balanced=True,
+        train_val_split=0.1,
+        num_workers=4,
+        data_path="./data",
+        load_data_on_every_trial=False,
+        seed=42,
+    )
