7593acadccf1e5bdcccb3b134534df6c23f9b32c
Marc Speckmann <marc-speckmann@web.de>
2023-08-21 16:08:11 +0200

    Applying dora specific settings

--- experiment_random_dora_2.py
+++ experiment_random_dora_2.py
@@ -272,10 +272,10 @@ if __name__ == "__main__":
     RESUME = False  # If True, the experiment is resumed from a previous checkpoint. Else a new experiment is started.
     SEED = 255462424  # Seed for reproducibility
     N_TRIALS = -1  # Number of trials to run. If -1, the number of trials is infinite.
-    WALLTIME_LIMIT = 6 * 60 * 60  # Time limit for the experiment in seconds. 6h
+    WALLTIME_LIMIT = 2 * 60 * 60  # Time limit for the experiment in seconds. 6h
     MAX_EPOCHS = 20  # Maximum number of epochs to train for.
     IMG_SIZE = 32  # Image size to use for the model. (IMG_SIZE, IMG_SIZE)
-    MAX_CONCURRENT_TRIALS = 1  # Maximum number of trials to run concurrently.
+    MAX_CONCURRENT_TRIALS = 3  # Maximum number of trials to run concurrently.
     DATASET_WORKER_PER_TRIAL = 4  # Number of workers to use for DataLoader.
     CUDAS_PER_TRIAL = 1  # Number of GPUs to use for each trial.
     CPU_PER_TRIAL = 4  # Number of CPUs to use for each trial.
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     OPTIMIZATION_METRIC = "val_accuracy_mean"  # Metric to optimize for.
     OPTIMIZATION_MODE = "max"  # Mode to optimize for.
     KEEP_N_BEST_MODELS = 1  # Number of best models to keep.
-    LOAD_DATA_ON_EVERY_TRIAL = False  # If True, the data is loaded for each trial. Used for distributed training.
+    LOAD_DATA_ON_EVERY_TRIAL = True  # If True, the data is loaded for each trial. Used for distributed training.
 
     HERE = Path(__file__).parent.absolute()  # Path to this file.
     DATA_PATH = HERE / "data"  # Path to the data directory.
